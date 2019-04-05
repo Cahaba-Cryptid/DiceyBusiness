4161c0f1e42a953da23e87fe0e08e170a40a0a14
Hayden <chfrank9011@gmail.com>
2019-04-05 13:49:09 -0500

    finished up project

--- app.js
+++ app.js
@@ -1,40 +1,73 @@
 
 $('body').css('background-color', 'gainsboro');
 $('#mulligan').css('margin-left', '35px');
+$('#mulligan').css('margin-right', '35px');
 
 let addDieBtn = document.getElementById('generate-dice');
 $(addDieBtn).click(generateDie)
 let diceOnBoard = []
+
 
 class D6 {
     constructor() {
         this.rollD6();
         this.div = $('<div class="die">' + this.rollOutcome + '</div>');
         $('#dice-tower').append(this.div)
+        this.div.css('text-align', 'center');
+        this.div.css('line-height', '100px');
+        this.div.css('background-color', 'rebeccaPurple');
+        this.div.css('height', '100px');
+        this.div.css('width', '100px');
+        this.div.css('float', 'left');
+        this.div.css('margin', '20px');
+        this.div.click(() => { 
+        this.reroll()
+        this.div.text(this.rollOutcome)
+        }) 
+        this.div.dblclick(() => {
+           this.div.remove();
+           diceOnBoard.splice(this.div, 1);
 
-        //styling
-        
-        $('.die').css('background-color', 'rebeccaPurple');
-        $('.die').css('height', '100px');
-        $('.die').css('width', '100px');
-        $('.die').css('float', 'left');
-        $('.die').css('margin', '20px');  
+        });
     }
     rollD6() {
         this.rollOutcome = Math.floor(Math.random() * 6 + 1);
     }
+    reroll() {
+        this.rollD6();
+    }
+
 }
 
 //roll function
 function generateDie() {
     let newD6 = new D6()
     diceOnBoard.push(newD6);
-    console.log(diceOnBoard);
 }
-//re-roll function in progress
-//will need to be used for mulligan button
+
+//re-roll dice displayed on screen
 function mulligan() {
-    
+    diceOnBoard.forEach(die => {
+        die.rollD6()
+        die.div.text(die.rollOutcome)
+
+    });
+
 }
+
+$('#mulligan').click(mulligan);
+
+$('#sum-dice').click(() => {
+
+    let diceTotals = 0;
+
+    for (let i = 0; i < diceOnBoard.length; i++) {
+    diceTotals += (diceOnBoard[i].rollOutcome)
+    }
+    alert(diceTotals);
+});
+
+
+
 
 
